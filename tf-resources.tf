aedc617e9a729dbfcc23496888b21f3fedb90edb
PoojaMishra007 <pooja12993@gmail.com>
2022-12-19 14:10:47 +0530

    added two files and removed one

--- tf-resources.tf
+++ tf-resources.tf
@@ -1,29 +1,35 @@
-# Create S3 Bucket Resource
-resource "aws_s3_bucket" "s3_bucket" {
-  bucket = var.bucket_name
-  acl    = "public-read"
-  policy = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "PublicReadGetObject",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": [
-                "s3:GetObject"
-            ],
-            "Resource": [
-                "arn:aws:s3:::${var.bucket_name}/*"
-            ]
-        }
-    ]
-  }  
-  EOF
-    website {
-      index_document = "index.html"
-      error_document = "error.html"
-    }
-    tags          = var.tags
-    force_destroy = true
+resource "aws_instance" "app_server" {
+  ami = var.appserver_imageid
+  instance_type = var.appserver_instance_type
+  key_name = "LinuxKeyPair"
+  security_groups = [ aws_security_group.allow_ssh_sg ]
+  vpc_security_group_ids = [ aws_security_group.allow_ssh_sg ]
+  
+ 
   }
+
+
+resource "aws_security_group" "allow_ssh_sg" {
+    name   = "Pooja_allow_ssh_sg"
+    description = "Allow ssh inbound traffic"
+
+
+    ingress  {
+      
+      description  = "Allow SSH"
+      from_port    = 22
+      protocol     = "tcp"
+      to_port      = 22
+      cidr_blocks  = ["0.0.0.0/0"]
+    } 
+
+    egress  {
+
+    
+      from_port = 0
+      protocol  = "-1"
+      to_port   = 0
+      cidr_blocks   = ["0.0.0.0/0"]
+    }
+    
+}
